d5d5dc4ec59e6daa66055b965dbcf5aad45d0584
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-16 21:44:44 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/springboot/example/SwaggerUIApplication.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerUIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerUIApplication.class, args);
+	}
+}
